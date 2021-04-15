86ee00bbf8f4d05715c057102a91e1f50ef6ec6e
Rubi Kuo <rubitiara@gmail.com>
2021-04-15 02:00:08 +0200

    updateCV

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -265,7 +265,7 @@ const Resume = () => {
           <Link
             data-aos="zoom-in"
             className="Resume__btn  Resume__btn-download"
-            to="Rubi-Kuo-CV.pdf"
+            to="RubiKuoCV.pdf"
             target="_blank"
             download
           >
